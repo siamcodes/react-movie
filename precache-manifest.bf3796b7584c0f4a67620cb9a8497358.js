068181808dcb5c7cfed565d9b1252ce71def67bb
Somkiat Jaidee <siamcodes@gmail.com>
2019-08-28 10:26:29 +0700

    Updates

---
+++ precache-manifest.bf3796b7584c0f4a67620cb9a8497358.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a20fad3b471599ef6586363b72740b6",
+    "url": "/react-movie/index.html"
+  },
+  {
+    "revision": "f84abf995b17d3d496fe",
+    "url": "/react-movie/static/css/2.01fc24db.chunk.css"
+  },
+  {
+    "revision": "cbdec99ea5726c74318d",
+    "url": "/react-movie/static/css/main.eae454fc.chunk.css"
+  },
+  {
+    "revision": "f84abf995b17d3d496fe",
+    "url": "/react-movie/static/js/2.188440e0.chunk.js"
+  },
+  {
+    "revision": "cbdec99ea5726c74318d",
+    "url": "/react-movie/static/js/main.a82fdc04.chunk.js"
+  },
+  {
+    "revision": "694c92a09239c247798b",
+    "url": "/react-movie/static/js/runtime~main.bee7937a.js"
+  }
+]);
